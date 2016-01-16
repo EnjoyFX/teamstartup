c3b0f7ec21d38784fdf0509001fc53cda8e619df
voytyshyn <voytyshyn@gmail.com>
2016-01-16 15:44:25 +0200

    SMART-21: added Karma configuration

--- Smart/tests/Smart.PL.WebUI.Frontend.Test/Scripts/app/pages/home/home.controller.spec.js
+++ Smart/tests/Smart.PL.WebUI.Frontend.Test/Scripts/app/pages/home/home.controller.spec.js
@@ -1,3 +1,12 @@
 ﻿/// <reference path="../../../libs/jasmine/jasmine.js" />
 
 /// <reference path="../../../../../../modules/smart.pl.webui.frontend/scripts/app/pages/home/home.controller.js" />
+
+describe("HomeController", function () {
+
+    it("1 + 1 = 2", function () {
+        var r = 1 + 1;
+        expect(r).toEqual(2);
+    });
+
+});
--- Smart/tests/Smart.PL.WebUI.Frontend.Test/Scripts/app/pages/home/presentation-bar.controller.spec.js
+++ Smart/tests/Smart.PL.WebUI.Frontend.Test/Scripts/app/pages/home/presentation-bar.controller.spec.js
@@ -1,7 +1,7 @@
 ﻿/// <reference path="../../../libs/jasmine/jasmine.js" />
 /// <reference path="../../../../../../modules/smart.pl.webui.frontend/scripts/app/pages/home/presentation-bar.controller.js" />
 
-describe("HomeController", function () {
+describe("PresentationBarController", function () {
 
     it("1 + 1 = 2", function () {
         var r = 1 + 1;

58105c5345d4420662f305380148d25edf78a0be
hyermakova <yermakova.galya@gmail.com>
2016-01-19 20:52:42 +0200

    SMART-14: added html layout for call to action section

--- Smart/modules/Smart.BL.Services/Concrete/CallToActionService.cs
+++ Smart/modules/Smart.BL.Services/Concrete/CallToActionService.cs
@@ -12,10 +12,10 @@ namespace Smart.BL.Services.Concrete
     {
         public CallToActionEntity GetDescription()
         {
-            return new CallToActionEntity { FindYourJobDescription = "FindYourJobDescription",
-                LaunchStartupDescription = "LaunchStartupDescription",
-                PostJobDescription = "PostJobDescription",
-                StartToInvestDescription = "StartToInvestDescription"
+            return new CallToActionEntity { FindYourJobDescription = "Smartup market is the best place to find a great freelance job or join the startup team to develop revolutionary technologies. It’s fast, fun and completely free for startups and candidates. Candidates can see salary and equity information upfront",
+                LaunchStartupDescription = "On Smartup market over 200 consortiums including Dave Morin, Elad Gil and Foundry Group invest in fast growing companies for A, B, C rounds. Startups can find 40,000 new candidates a week — over half are engineers, designers or product managers",
+                PostJobDescription = "Smartup Job market allows startups, employers and candidates to meet through simple, Tinder-like matching. It’s simply to hire qualified and professional talents to meet your job requirements",
+                StartToInvestDescription = "Investors care about the team, the traction you’ve achieved so far, the size of the market you’re addressing, and the entry barriers you can create"
             };
         }
     }

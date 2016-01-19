868a5e5f14fcb05d56ed63b0a84344db9fbfdcd7
hyermakova <yermakova.galya@gmail.com>
2016-01-19 20:51:56 +0200

    SMART-11: added html layout for statistic section

--- Smart/modules/Smart.PL.WebUI.Frontend/Scripts/app/pages/home/statistic.controller.js
+++ Smart/modules/Smart.PL.WebUI.Frontend/Scripts/app/pages/home/statistic.controller.js
@@ -9,7 +9,8 @@
     function StatisticController($scope, statisticService) {
         var vm = this;
 
-        vm.statistic = [];
+        vm.statisticLeft = [];
+        vm.statisticRight = [];
 
         activate();
 
@@ -18,7 +19,9 @@
             statisticService.getStatistic()
                 .then(function (response) {
                     $scope.$applyAsync(function () {
-                        vm.statistic = response.data.statistic;
+                        var arr = response.data.statistic;
+                        vm.statisticLeft = arr.slice(0, arr.length/2);
+                        vm.statisticRight = arr.slice(arr.length / 2 - 1, arr.length - 1);
                     });
                 }, function (response) {
                     console.log("[statistic.controller] statisticService.getStatistic() failed");
